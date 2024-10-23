4c62f8817c48748e0005f947258cb3ca9d878d87
rockywu 吴佳雷 <wjl19890427@hotmail.com>
2024-10-23 11:48:22 +0800

    修改

--- job.py
+++ job.py
@@ -1,16 +1,22 @@
-from pymilvus import MilvusClient,CollectionSchema,DataType
+from pymilvus import MilvusClient,CollectionSchema,DataType,FieldSchema
 from sentence_transformers import SentenceTransformer
 from mysql_helper import query_mysql
+import torch
+
+# 初始化模型并将其加载到 CPU 或 GPU
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# 定义文本向量化模型 # 定义模型并载入到指定设备
+#model = SentenceTransformer("all-MiniLM-L6-v2").to(device)
+print('device', device)
 
 # 初始化 MilvusClient
 client = MilvusClient(
     uri='http://home.wujialei.com:19530',
-    token="root:Rockywu0427",
-    db_name="my_db"
+    user="root",
+    password="Rockywu0427",
+    db_name="mf_db"
 )
 
-# 定义文本向量化模型
-model = SentenceTransformer("all-MiniLM-L6-v2")
 
 # 定义集合名称和 Schema
 collection_name = "room_embeddings"
@@ -33,65 +39,8 @@ collection_schema = CollectionSchema(
 if not client.has_collection(collection_name):
    client.create_collection(collection_name, schema=collection_schema, shards_num=2)
 
-# 分页查询和处理
-page_size = 100
-offset = 0
-while True:
-    # SQL 查询语句
-    sql_query = f"""
-        SELECT 
-            r.store_code,
-            r.store_name,
-            r.room_unique_code,
-            r.room_number,
-            r.price,
-            r.building_number,
-            r.building_name,
-            r.floor,
-            r.long_term_type_code,
-            r.long_term_type_name,
-            r.area,
-            r.orientation,
-            r.light_status,
-            r.decoration,
-            s.city_code,
-            s.city_name,
-            s.region_code,
-            s.region_name,
-            s.store_address,
-            s.latitude,
-            s.longitude
-        FROM
-            rooms r
-        JOIN
-            stores s ON r.store_code = s.store_code
-        LIMIT {page_size} OFFSET {offset}
-    """
 
-    # 查询数据
-    results = query_mysql(sql_query)
-    if not results:
-        break  # 如果没有更多结果，则退出循环
+client.close()
 
-    # 插入每页数据
-    for record in results:
-        combined_text = f"{record['region_name']} {record['store_address']} {record['store_name']} {record['city_name']}"
-        embedding = model.encode(combined_text).tolist()
 
-        insert_data = [
-            record['room_unique_code'],
-            record['store_code'],
-            record['region_name'],
-            record['store_address'],
-            record['store_name'],
-            record['city_name'],
-            embedding
-        ]
 
-        client.insert(collection_name, [insert_data])
-
-    # 递增偏移量，以便下次检索下一页
-    offset += page_size
-
-# 在最后刷新集合以确保数据持久化
-client.flush([collection_name])
